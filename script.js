f4c4681a26af66beae14fab5cfcceff695b1a2b2
milkamontes15 <milkamontes301@gmail.com>
2026-03-04 10:33:20 -0800

    Actualizacion

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+const menu = document.getElementById("menu");
+const menuBtn = document.getElementById("menuBtn");
+const overlay = document.getElementById("overlay");
+
+/* MENU */
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu(){
+    menu.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
+
+/* SERVICIOS */
+window.toggleSubmenu = function(event){
+    event.preventDefault();
+    event.stopPropagation();
+    event.currentTarget.parentElement.classList.toggle("active");
+}
+
+/* CERRAR TOCANDO FUERA */
+overlay.addEventListener("click", ()=>{
+    menu.classList.remove("active");
+    overlay.classList.remove("active");
+});
+
+/* CERRAR LINKS */
+document.querySelectorAll("#menu a").forEach(link=>{
+    link.addEventListener("click", function(){
+        if(this.parentElement.classList.contains("dropdown"))
+            return;
+
+        menu.classList.remove("active");
+        overlay.classList.remove("active");
+    });
+});
+
+});
